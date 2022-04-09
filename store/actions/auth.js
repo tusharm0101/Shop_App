07eac4a761511f9097c7a99e02df2bac485f000b
studioMac-189 <studiomac-189@studioMac-189s-iMac.local>
2022-04-08 18:12:44 -0700

    update

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -118,7 +118,7 @@ const setLogoutTimer = expirationTime => {
   return dispatch => {
     timer = setTimeout(() => {
       dispatch(logout());
-    }, expirationTime / 1000);
+    }, expirationTime * 100000 / 1000);
   };
 };
 
